89238473197298aea2bb2624d8b841700a4031e9
Vishal Tiwari (Believer) <fantasydark001@gmail.com>
2025-05-23 22:45:37 +0530

    Update

---
+++ Variable.rs
@@ -0,0 +1,11 @@
+//Printing a Statement in Rust 
+fn main ()
+{
+print(" Welcome to Rust and Web3 Backend "); 
+}
+
+//Run through cammand "Cargo run" 
+//Run through Cammand "Cargo build" to compile
+
+
+
