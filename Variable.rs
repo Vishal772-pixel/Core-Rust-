146aedcb1b8ee8c1a326bc028837183f8d821aea
Vishal Tiwari (Believer) <fantasydark001@gmail.com>
2025-05-23 22:59:18 +0530

    Update Variable.rs

--- Variable.rs
+++ Variable.rs
@@ -8,4 +8,20 @@ print(" Welcome to Rust and Web3 Backend ");
 //Run through Cammand "Cargo build" to compile
 
 
+// Get about signed and unsigned integers 
+limit of variables i8,i16,i32 and all
+f
+fn main ()
+{
+int x:i8= 5 ;// signed integer can be negative or positive 
+int y:u32=1000 // unsigned integers can't be negative 
+int z:f32= 100.8 // floating value or decimals 
+
+print!("x:{},y:{},z:{}", x,y,z);
+}
+
+
+}
+
+
 
