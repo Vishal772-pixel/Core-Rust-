81df57870559531320d1b49924af37a5b9bf7e34
Vishal Tiwari (Believer) <fantasydark001@gmail.com>
2025-05-29 18:17:42 +0530

    Update Strings.rs

--- Strings.rs
+++ Strings.rs
@@ -14,6 +14,8 @@ let greetings:String=String::from("Hello world");
 printf("{}",greeting);
 
 //Ways to interate a String (2 ways as i know) 
+
+//First way to iterate a string 
 let char1:Option<char>=greeting.char().nth(0);
 print!("char1:{}",char1.unwrap());
 }
