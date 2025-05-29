288b2c3ec50b025592d2a6d92d1d76c6555c20fc
Vishal Tiwari (Believer) <fantasydark001@gmail.com>
2025-05-29 18:13:25 +0530

    Update

---
+++ Strings.rs
@@ -0,0 +1,14 @@
+//Tricky Datatype of Rust ( string)
+
+
+//First way to initialise a String 
+fn main ()
+{
+let greetings:&str="Hello world";
+print!("{}",greeting);
+
+//second way to initialise the string
+
+fn main()
+let greetings:String=String::from("Hello world");
+
